1a7aa37a50bc28f8186ebcc294238b31701751dc
emontecalvo <emilymontecalvo@gmail.com>
2017-01-10 13:42:24 -0800

    experimenting with zghtml

--- client/js/components/home.js
+++ client/js/components/home.js
@@ -2,17 +2,25 @@ import React from 'react';
 import {connect} from 'react-redux';
 import Card from './card';
 import Navbar from './navbar';
+import Zen1 from './zen1';
+
 
 class Home extends React.Component {
+	testfunc() {
+		console.log("here");
+	}
+
 	render() {
 		return <div>
 			<Navbar />
 			<h1>the garden project</h1>
+			<button onClick={this.testfunc}>change html</button>
 			<div className="card_list">
 				<Card />
 				<Card />
 				<Card />
 				<Card />
+				<Zen1 />
 			</div>
 
 		</div>;
